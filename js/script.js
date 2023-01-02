392beb5b71e7120f996303d839b9955573fe4cd8
monika.sobczak <monika.sobczak@vulcan.edu.pl>
2023-01-02 11:59:57 +0100

    Add random number and print message with random computer move

--- js/script.js
+++ js/script.js
@@ -8,10 +8,15 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-var computerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce';
+}
 
-var playerMove;
-playerMove = "papier";
-printMessage('Zagrałeś ' + playerMove + '! Brawo!!! Wygrałeś :)' )
+printMessage('Mój ruch: ' + computerMove);
